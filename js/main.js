e314185eb80703aaabd6fa86555b462869ca06e0
m3lty <adam@kd3ts.com>
2018-04-29 12:08:29 -0400

    A lot.

--- js/main.js
+++ js/main.js
@@ -2,27 +2,39 @@ $(document).ready(function(){
   $("#title1").fadeIn(1000).removeClass("hidden");
   $("#title2").fadeIn(2000).removeClass("hidden");
   $("#title3").fadeIn(3000).removeClass("hidden");
+  $(".hiddenLink").fadeIn(4000).removeClass("hidden");
 });
-$.fn.isOnScreen = function(){
 
-    var win = $(window);
-
-    var viewport = {
-        top : win.scrollTop(),
-        left : win.scrollLeft()
-    };
-    viewport.right = viewport.left + win.width();
-    viewport.bottom = viewport.top + win.height();
-
-    var bounds = this.offset();
-    bounds.right = bounds.left + this.outerWidth();
-    bounds.bottom = bounds.top + this.outerHeight();
-
-    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
-
-};
-$(window).scroll(function(){
-    if($("#profilepic").isOnScreen){
-      $("#profilepic").fadeIn(1000).removeClass("hidden");
+$(document).scroll(function () {
+    var y = $(this).scrollTop();
+    if (y > 300) {
+        $('.skillContainer').fadeIn(500).toggleClass("visible");
     }
 });
+
+
+
+
+function ScrollTo(target, speed, timeout) {
+
+    if (!timeout) timeout = 0;
+    if (!speed) speed = 1000;
+  
+    if (typeof target !== "undefined") {
+      //target = target.replace("#", ".");
+      //target = $(document.body).find(target);
+      setTimeout(function() {
+          console.log($(target).offset().top);
+        $('html,body').animate({
+          scrollTop: $(target).offset().top
+        }, speed);
+      }, timeout);
+    }
+  }
+  
+  $(".hiddenLink").click(function(e) {
+    // Prevent a page reload when a link is pressed
+    e.preventDefault();
+    // Call the scroll function
+    ScrollTo($(this).attr("href"), 0, 0); // target, speed, timeout
+  });
